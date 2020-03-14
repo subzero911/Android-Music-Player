9563c5a71708b61eee263a3ddefd85338a25b8c1
subzero911 <subzero911@yandex.ru>
2020-03-14 21:12:31 +0300

    initial

---
+++ app/src/main/java/com/molchanovsky/musicplayer/MainActivity.java
@@ -0,0 +1,80 @@
+package com.molchanovsky.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    ImageView playPauseIcon;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playPauseIcon = findViewById(R.id.playIconImageView);
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.c_wilson_sugarhoneyicetea);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser){
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekBar.setProgress(mediaPlayer.getCurrentPosition());
+            }
+        }, 0, 1000);
+    }
+
+    public void previousTrack(View view) {
+        seekBar.setProgress(0);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.ic_play_arrow_yellow_24dp);
+    }
+
+    public void nextTrack(View view) {
+        seekBar.setProgress(mediaPlayer.getDuration());
+        mediaPlayer.seekTo(mediaPlayer.getDuration());
+        mediaPlayer.pause();
+        playPauseIcon.setImageResource(R.drawable.ic_play_arrow_yellow_24dp);
+    }
+
+    public void play(View view) {
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            playPauseIcon.setImageResource(R.drawable.ic_play_arrow_yellow_24dp);
+        }
+        else {
+            mediaPlayer.start();
+            playPauseIcon.setImageResource(R.drawable.ic_pause_yellow_24dp);
+        }
+    }
+}
